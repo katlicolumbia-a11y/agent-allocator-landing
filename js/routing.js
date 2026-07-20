2a231fbb4fc64b451f2bb32339c2928763b66615
katlicolumbia-a11y <kat.li.columbia@gmail.com>
2026-07-19 22:40:24 -0400

    Shorten console scroll sequence

--- js/routing.js
+++ js/routing.js
@@ -1603,7 +1603,7 @@
       trigger: stage,
       pin: true,
       start: 'top top',
-      end: '+=8200',
+      end: '+=4200',
       scrub: 0.5,
       anticipatePin: 1,
       onRefresh: function () { layoutPage2(); master(proxy.p); }
